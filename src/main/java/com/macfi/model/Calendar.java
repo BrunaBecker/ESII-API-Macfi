b3c7a399e0041bcba151c42d076e24d3398a7b51
Asunnya <asunnyya@gmail.com>
2023-11-04 15:20:34 -0300

    add calendar models

--- src/main/java/com/macfi/model/Calendar.java
+++ src/main/java/com/macfi/model/Calendar.java
@@ -1,4 +1,23 @@
 package com.macfi.model;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class Calendar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToMany
+    @JoinTable(name = "calendar_id", joinColumns = @JoinColumn(name = "calendarId", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "eventId", referencedColumnName = "id"))
+    private List<Event> events;
 }
--- src/main/java/com/macfi/model/Event.java
+++ src/main/java/com/macfi/model/Event.java
@@ -1,2 +1,33 @@
-package com.macfi.model;public class Event {
+package com.macfi.model;
+
+import com.macfi.model.utils.enums_class.eventStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name; //sera a data
+
+    private Date date;
+
+    private String description;
+
+    @OneToOne
+    private Classroom classroom;
+
+    private eventStatus status;
+
+    @ManyToMany(mappedBy = "events")
+    private List<Calendar> calendar;
 }
