5c969d146ebeef84dc380e76d7e0c519b943f126
Asunnya <asunnyya@gmail.com>
2023-11-10 16:40:59 -0300

    feat: calendar dto services and controller

--- src/main/java/com/macfi/controller/CalendarController.java
+++ src/main/java/com/macfi/controller/CalendarController.java
@@ -1,9 +1,11 @@
 package com.macfi.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.macfi.payload.CalendarDto;
+import com.macfi.service.CalendarService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("http://localhost:8080")
 @RestController
@@ -11,4 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalendarController {
 
 
+    @Autowired
+    private CalendarService calendarService;
+
+    @GetMapping
+    public CalendarDto getCalendar(@Valid @RequestBody CalendarDto calendarDto) {
+        return calendarService.getCalendar(calendarDto);
+    }
+
+    @PostMapping
+    public CalendarDto createCalendar(@Valid @RequestBody CalendarDto calendarDto) {
+        return calendarService.createCalendar(calendarDto);
+    }
+
+    @PutMapping
+    public CalendarDto updateCalendar(@Valid @RequestBody CalendarDto calendarDto) {
+        return calendarService.updateCalendar(calendarDto);
+    }
+
+    @GetMapping("byId/{id}")
+    public CalendarDto getById(@PathVariable("id") Long id) {
+        return calendarService.getById(id);
+    }
 }
