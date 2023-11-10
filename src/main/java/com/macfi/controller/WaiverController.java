694d3e9b475bf03fc67d2011c2e040b414f4c9a8
Asunnya <asunnyya@gmail.com>
2023-11-10 15:25:01 -0300

    feat: add waiverDTO and modified all controller/services class

--- src/main/java/com/macfi/controller/WaiverController.java
+++ src/main/java/com/macfi/controller/WaiverController.java
@@ -1,8 +1,12 @@
 package com.macfi.controller;
 
 import com.macfi.model.Waiver;
+import com.macfi.payload.WaiverDto;
 import com.macfi.service.WaiverService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("http://localhost:8080")
@@ -13,15 +17,27 @@ public class WaiverController {
     @Autowired
     private WaiverService waiverService;
 
-
+    @Operation(
+            summary = "Create Waiver REST API",
+            description = "Create Waiver REST API is used to save post into database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
     @PostMapping
-    public Waiver createWaiver(@RequestBody Waiver waiver) {
-        return waiverService.createWaiver(waiver);
+    public ResponseEntity<WaiverDto> createWaiver(@RequestBody WaiverDto waiverDto) {
+        return new ResponseEntity<>(waiverService.createWaiver(waiverDto), org.springframework.http.HttpStatus.CREATED);
     }
 
     @GetMapping("byStudentAndClassroom") //http://localhost:8080/waiver/byStudentAndClassroom?idStudent=1&idClassroom=1
-    public Waiver getWaiverByStudentAndClassroom(@RequestParam Long idStudent, @RequestParam Long idClassroom) {
-        return waiverService.getWaiverByStudentAndClassroom(idStudent, idClassroom);
+    public ResponseEntity<WaiverDto> getWaiverByStudentAndClassroom(@RequestParam Long idStudent, @RequestParam Long idClassroom) {
+        return ResponseEntity.ok(waiverService.getWaiverByStudentAndClassroom(idStudent, idClassroom));
+    }
+
+    @GetMapping("{id}") //http://localhost:8080/waiver/1
+    public ResponseEntity<WaiverDto> getWaiverById(@PathVariable Long id) {
+        return ResponseEntity.ok(waiverService.getWaiverById(id));
     }
 
 
--- src/main/java/com/macfi/service/WaiverService.java
+++ src/main/java/com/macfi/service/WaiverService.java
@@ -1,6 +1,8 @@
 package com.macfi.service;
 
 import com.macfi.model.Waiver;
+import com.macfi.modelMapper.modelMapping;
+import com.macfi.payload.WaiverDto;
 import com.macfi.repository.WaiverRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +11,18 @@ public class WaiverService {
 
     public WaiverRepository waiverRepository;
 
-    public Waiver createWaiver(Waiver waiver) {
-        return waiverRepository.save(waiver);
+    public WaiverDto createWaiver(WaiverDto waiverDto) {
+        return modelMapping.getInstance().mapToDto(waiverRepository.save(modelMapping.getInstance().mapToEntity(waiverDto, Waiver.class)), WaiverDto.class);
     }
 
-    public Waiver getWaiverByStudentAndClassroom(Long id, Long idClassroom) {
-        return waiverRepository.findByStudentAndClassroom(id, idClassroom);
+    public WaiverDto getWaiverByStudentAndClassroom(Long id, Long idClassroom) {
+        return modelMapping.getInstance().mapToDto(waiverRepository.findByStudentIdAndClassroomId(id, idClassroom), WaiverDto.class);
     }
+
+    public WaiverDto getWaiverById(Long id) {
+        return modelMapping.getInstance().mapToDto(waiverRepository.findById(id).isPresent(), WaiverDto.class);
+    }
+
 
 
 }
