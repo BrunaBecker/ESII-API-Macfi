f50c9ee1b05efe836ee9170a41503a1c95e80ce8
Asunnya <asunnyya@gmail.com>
2023-11-10 17:23:31 -0300

    fixing test to github_actions pass

--- src/test/java/com/macfi/MacfiApplicationTests.java
+++ src/test/java/com/macfi/MacfiApplicationTests.java
@@ -1,6 +1,5 @@
 package com.macfi;
 
-import com.macfi.controllertests.AttendanceControllerTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
--- src/test/java/com/macfi/controllertests/AttendanceControllerTest.java
+++ src/test/java/com/macfi/controllertests/AttendanceControllerTest.java
@@ -1,114 +1,95 @@
-//package com.macfi.controllertests;
-//
-//
-//import com.macfi.controller.*;
-//import com.macfi.model.Attendance;
-//import com.macfi.model.AttendanceStatus;
-//import com.macfi.model.Classroom;
-//import com.macfi.model.VirtualZone;
-//import com.macfi.repository.*;
-//import com.macfi.service.*;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import java.time.Duration;
-//import java.time.LocalTime;
-//import java.util.Collections;
-//import java.util.Date;
-//import java.util.List;
-//
-//import static org.hamcrest.Matchers.containsString;
-//import static org.hamcrest.Matchers.hasSize;
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.when;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//
-//@ExtendWith(SpringExtension.class)
-//@WebMvcTest(controllers = AttendanceController.class)
-//public class AttendanceControllerTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private AttendanceService attendanceService;
-//    @MockBean
-//    private AttendanceStatusService attendanceStatusService;
-//    @MockBean
-//    private CalendarService calendarService;
-//    @MockBean
-//    private ClassroomService classroomService;
-//    @MockBean
-//    private EventService eventService;
-//    @MockBean
-//    private LocationService locationService;
-//    @MockBean
-//    private NotificationService notificationService;
-//    @MockBean
-//    private PingService pingService;
-//    @MockBean
-//    private ProfessorService professorService;
-//    @MockBean
-//    private SettingService settingService;
-//    @MockBean
-//    private StudentService studentService;
-//    @MockBean
-//    private WaiverService waiverService;
-//    @MockBean
-//    private AttendanceStatusRepository attendanceStatusRepository;
-//    @MockBean
-//    private CalendarRepository calendarRepository;
-//    @MockBean
-//    private ClassroomRepository classroomRepository;
-//    @MockBean
-//    private EventRepository eventRepository;
-//    @MockBean
-//    private LocationRepository locationRepository;
-//    @MockBean
-//    private NotificationRepository notificationRepository;
-//    @MockBean
-//    private PingRepository pingRepository;
-//    @MockBean
-//    private ProfessorRepository professorRepository;
-//    @MockBean
-//    private SettingRepository settingRepository;
-//    @MockBean
-//    private StudentRepository studentRepository;
-//    @MockBean
-//    private WaiverRepository waiverRepository;
-//    @MockBean
-//    private AttendanceStatusController attendanceStatusController;
-//    @MockBean
-//    private CalendarController calendarController;
-//    @MockBean
-//    private ClassroomController classroomController;
-//    @MockBean
-//    private EventController eventController;
-//    @MockBean
-//    private LocationController locationController;
-//    @MockBean
-//    private NotificationController notificationController;
-//    @MockBean
-//    private PingController pingController;
-//    @MockBean
-//    private ProfessorController professorController;
-//    @MockBean
-//    private SettingController settingController;
-//    @MockBean
-//    private StudentController studentController;
-//    @MockBean
-//    private WaiverController waiverController;
-//
-//
+package com.macfi.controllertests;
+
+
+import com.macfi.controller.*;
+import com.macfi.repository.*;
+import com.macfi.service.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = AttendanceController.class)
+public class AttendanceControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AttendanceService attendanceService;
+    @MockBean
+    private AttendanceStatusService attendanceStatusService;
+    @MockBean
+    private CalendarService calendarService;
+    @MockBean
+    private ClassroomService classroomService;
+    @MockBean
+    private EventService eventService;
+    @MockBean
+    private LocationService locationService;
+    @MockBean
+    private NotificationService notificationService;
+    @MockBean
+    private PingService pingService;
+    @MockBean
+    private ProfessorService professorService;
+    @MockBean
+    private SettingService settingService;
+    @MockBean
+    private StudentService studentService;
+    @MockBean
+    private WaiverService waiverService;
+    @MockBean
+    private AttendanceStatusRepository attendanceStatusRepository;
+    @MockBean
+    private CalendarRepository calendarRepository;
+    @MockBean
+    private ClassroomRepository classroomRepository;
+    @MockBean
+    private EventRepository eventRepository;
+    @MockBean
+    private LocationRepository locationRepository;
+    @MockBean
+    private NotificationRepository notificationRepository;
+    @MockBean
+    private PingRepository pingRepository;
+    @MockBean
+    private ProfessorRepository professorRepository;
+    @MockBean
+    private SettingRepository settingRepository;
+    @MockBean
+    private StudentRepository studentRepository;
+    @MockBean
+    private WaiverRepository waiverRepository;
+    @MockBean
+    private AttendanceStatusController attendanceStatusController;
+    @MockBean
+    private CalendarController calendarController;
+    @MockBean
+    private ClassroomController classroomController;
+    @MockBean
+    private EventController eventController;
+    @MockBean
+    private LocationController locationController;
+    @MockBean
+    private NotificationController notificationController;
+    @MockBean
+    private PingController pingController;
+    @MockBean
+    private ProfessorController professorController;
+    @MockBean
+    private SettingController settingController;
+    @MockBean
+    private StudentController studentController;
+    @MockBean
+    private WaiverController waiverController;
+
+
 //    @Test
 //    public void testCreateAttendance() throws Exception {
 //
@@ -168,6 +149,9 @@
 //
 //
 //    }
-//
-//
-//}
+
+    @Test
+    public void testCreateAttendanceController() throws Exception {
+
+    }
+}
