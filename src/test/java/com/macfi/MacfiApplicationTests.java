5072767f52ed2939e2fed0b8a4a7b26554924937
Asunnya <asunnyya@gmail.com>
2023-11-10 18:33:43 -0300

    fixing test to github_actions pass

--- src/test/java/com/macfi/MacfiApplicationTests.java
+++ src/test/java/com/macfi/MacfiApplicationTests.java
@@ -1,8 +1,13 @@
 package com.macfi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class MacfiApplicationTests {
 
@@ -10,4 +15,9 @@ class MacfiApplicationTests {
     void contextLoads() {
     }
 
+    @TestConfiguration
+    static class MyTestConfig {
+
+
+    }
 }
