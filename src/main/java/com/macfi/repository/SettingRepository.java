072b1cb908047199c35c2d314a6164cb377713b1
Asunnya <asunnyya@gmail.com>
2023-11-21 00:35:30 -0300

    :sparkles: setting repository add findByPersonId

--- src/main/java/com/macfi/repository/SettingRepository.java
+++ src/main/java/com/macfi/repository/SettingRepository.java
@@ -4,8 +4,13 @@ import com.macfi.model.Setting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface SettingRepository extends JpaRepository<Setting, Long> {
 
     @Query("select s from Setting s where s.person.register.identifier = :identify")
-    Setting findByPersonIdentifier(Long identify);
+    Setting findByPersonIdentifier(String identify);
+
+    @Query("select s from Setting s where s.person.id = ?1")
+    Optional<Setting> findByPersonId(Long id);
 }
